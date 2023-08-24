edb63adf384ddb3e964552dc2ebe9d307c9cc6f3
joker53-1 <1205637346@qq.com>
2023-08-24 15:06:04 +0000

    modify

--- build.rs
+++ build.rs
@@ -1,21 +1,8 @@
 
+use protobuf_build::Builder;
 fn main() {
-    let includes = &[std::path::PathBuf::from("proto")];
-    let mut protos = Vec::new();
-    for include in includes {
-        for file in std::fs::read_dir(include).unwrap() {
-            let file = file.unwrap();
-            if file.file_type().unwrap().is_dir() {
-                continue;
-            }
-            let path = file.path();
-            if path.extension().unwrap() == "proto" {
-                protos.push(path);
-            }
-        }
-    } 
-    prost_build::compile_protos(&protos, includes).unwrap();
-    for p in protos {
-        println!("cargo:rerun-if-changed={}", p.display());
-    }
+    Builder::new()
+        .search_dir_for_protos("proto")
+        .append_to_black_list("eraftpb")
+        .generate()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+#[allow(dead_code)]
+#[allow(unknown_lints)]
+#[allow(clippy::all)]
+#[allow(renamed_and_removed_lints)]
+#[allow(bare_trait_objects)]
+#[allow(deprecated)]
+mod protos {
+    include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+    use raft_proto::eraftpb;
+}
+
+pub use protos::*;
+
 pub mod storage;
 pub mod server;
 pub mod coprocessor;
